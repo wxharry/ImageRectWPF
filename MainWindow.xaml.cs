062120855f3a278b1fde2b8d1260ed43b40939ba
Harry <xiaohanwu12@gmail.com>
2023-02-11 17:01:21 -0500

    fix: transparent background when saving and rectangle can move out

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -58,15 +59,8 @@ namespace ImageRectWPF
                 image.UriSource = new Uri(openFileDialog.FileName);
                 image.EndInit();
 
-                // Create an image control to display the image
-                Image imageControl = new Image();
-                imageControl.Source = image;
-
-                // specify image maxheight and maxwidth
-                // var toolbarHeight = SystemParameters.PrimaryScreenHeight - SystemParameters.FullPrimaryScreenHeight - SystemParameters.WindowCaptionHeight;
-                imageControl.MaxHeight = this.ActualHeight - TopBar.ActualHeight - 40; // the height is off a little; guessing causing by the topbar
-                imageControl.MaxWidth = this.ActualWidth;
-                MyCanvas.Children.Add(imageControl);
+                ImageBrush brush = new(image);
+                MyCanvas.Background = brush;
             }
         }
 
@@ -80,6 +74,12 @@ namespace ImageRectWPF
             Canvas.SetZIndex(selectedRectangle, maxZindex + 1);
             selectedRectangle.Stroke = System.Windows.Media.Brushes.SkyBlue;
             selectedRectangle.StrokeThickness = 2;
+            // get the size and position of the selectedRectangle
+            rectLeft = Canvas.GetLeft(selectedRectangle);
+            rectTop = Canvas.GetTop(selectedRectangle);
+            rectWidth = selectedRectangle.Width;
+            rectHeight = selectedRectangle.Height;
+            selectedRectangle.CaptureMouse();
         }
         private void Unselect_Rectangle(Rectangle rect)
         {
@@ -94,8 +94,7 @@ namespace ImageRectWPF
 
         private void MyCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            Point clickedPoint = e.GetPosition(this);
-            clickedPoint.Y -= TopBar.ActualHeight;
+            Point clickedPoint = e.GetPosition(MyCanvas);
             System.Diagnostics.Debug.WriteLine("click on " + e.Source);
             Rectangle clickedRectangle = null;
             if (e.Source is Rectangle)
@@ -106,12 +105,6 @@ namespace ImageRectWPF
             {
                 Unselect_Rectangle(selectedRectangle);
                 Select_Rectangle(clickedRectangle);
-                // get the size and position of the selectedRectangle
-                rectLeft = Canvas.GetLeft(selectedRectangle);
-                rectTop = Canvas.GetTop(selectedRectangle);
-                rectWidth = selectedRectangle.Width;
-                rectHeight = selectedRectangle.Height;
-
                 //Check if the user clicked on a resize handle
                 if (Math.Abs(clickedPoint.Y - rectTop - rectHeight) <= 10 && Math.Abs(clickedPoint.X - rectLeft - rectWidth) <= 10)
                 {
@@ -153,38 +146,47 @@ namespace ImageRectWPF
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
-                Point currentPoint = e.GetPosition(this);
-                currentPoint.Y -= TopBar.ActualHeight;
-                // resize on right bottom corner
+                Point currentPoint = e.GetPosition(MyCanvas);
                 if (selectedRectangle != null)
                 {
                     // resize a rectangle
+                    // resize on right bottom corner
                     if (isResizing)
                     {
                         // Update the size and position of the selected rectangle
-                        selectedRectangle.Width = Math.Abs(currentPoint.X - rectLeft);
-                        selectedRectangle.Height = Math.Abs(currentPoint.Y - rectTop);
-                        Canvas.SetLeft(selectedRectangle, Math.Min(rectLeft, currentPoint.X));
-                        Canvas.SetTop(selectedRectangle, Math.Min(rectTop, currentPoint.Y));
+                        double x = Math.Min(currentPoint.X, rectLeft);
+                        double y = Math.Min(currentPoint.Y, rectTop);
+                        double width = Math.Max(currentPoint.X, rectLeft) - x;
+                        double height = Math.Max(currentPoint.Y, rectTop) - y;
+                        selectedRectangle.Width = Math.Min(width, MyCanvas.ActualWidth - x);
+                        selectedRectangle.Height = Math.Min(height, MyCanvas.ActualHeight - y);
+                        Canvas.SetLeft(selectedRectangle, x);
+                        Canvas.SetTop(selectedRectangle, y);
                     }
                     // move a rectangle
                     if (isMoving)
-                    {      
+                    {
+                        System.Diagnostics.Debug.WriteLine("ismoving");
                         // keep updating the position of the rectangle
                         double left = Canvas.GetLeft(selectedRectangle);
                         double top = Canvas.GetTop(selectedRectangle);
-                        Canvas.SetLeft(selectedRectangle, left + (currentPoint.X - startPoint.X));
-                        Canvas.SetTop(selectedRectangle, top + (currentPoint.Y - startPoint.Y));
+                        double x = Math.Max(0, Math.Min(currentPoint.X - startPoint.X + left, MyCanvas.ActualWidth - selectedRectangle.Width));
+                        double y = Math.Max(0, Math.Min(currentPoint.Y - startPoint.Y + top, MyCanvas.ActualHeight - selectedRectangle.Height));
+                        Canvas.SetLeft(selectedRectangle, x);
+                        Canvas.SetTop(selectedRectangle, y);
                         startPoint = currentPoint;
                     }
                     // draw a rectangle
                     if (isDrawing)
                     {
-                        // Update the size and position of the rectangle
-                        selectedRectangle.Width = Math.Abs(currentPoint.X - startPoint.X);
-                        selectedRectangle.Height = Math.Abs(currentPoint.Y - startPoint.Y);
-                        Canvas.SetLeft(selectedRectangle, Math.Min(startPoint.X, currentPoint.X));
-                        Canvas.SetTop(selectedRectangle, Math.Min(startPoint.Y, currentPoint.Y));
+                        double x = Math.Min(currentPoint.X, startPoint.X);
+                        double y = Math.Min(currentPoint.Y, startPoint.Y);
+                        double width = Math.Max(currentPoint.X, startPoint.X) - x;
+                        double height = Math.Max(currentPoint.Y, startPoint.Y) - y;
+                        selectedRectangle.Width = Math.Min(width, MyCanvas.ActualWidth - x);
+                        selectedRectangle.Height = Math.Min(height, MyCanvas.ActualHeight - y);
+                        Canvas.SetLeft(selectedRectangle, x);
+                        Canvas.SetTop(selectedRectangle, y);
                     }
 
                 }
@@ -201,6 +203,7 @@ namespace ImageRectWPF
             if (selectedRectangle != null)
             {
                 Canvas.SetZIndex(selectedRectangle, maxZindex+1);
+                selectedRectangle.ReleaseMouseCapture();
                 ++maxZindex;
             }
         }
@@ -231,6 +234,35 @@ namespace ImageRectWPF
         private void DeleteBtn_Click(object sender, RoutedEventArgs e)
         {
             Delete_SelectedRectangle();
+        }
+        private void SaveImage()
+        {
+            // Open a file dialog to select the image
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new();
+            saveFileDialog.Title = "Save picture as ";
+            saveFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                RenderTargetBitmap renderTargetBitmap = new((int)MyCanvas.ActualWidth, (int)MyCanvas.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+                renderTargetBitmap.Render(MyCanvas);
+
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    encoder.Save(fileStream);
+                }
+            }
+
+        }
+        private void Save_Image(object sender, RoutedEventArgs e)
+        {
+            // check if canvas has no child
+            if (MyCanvas.Children.Count == 0) return;
+            Unselect_Rectangle(selectedRectangle);
+            SaveImage();
         }
     }
 }
